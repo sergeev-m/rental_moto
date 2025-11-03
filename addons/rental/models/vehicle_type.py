163634535fa779c1441196104c6c3113710027f1
Mikhail Sergeev <server-15@yandex.ru>
2025-11-03 21:33:33 +0500

    [add] vehicle.type constrains

--- addons/rental/models/vehicle_type.py
+++ addons/rental/models/vehicle_type.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 
 class VehicleType(models.Model):
@@ -6,3 +7,22 @@ class VehicleType(models.Model):
     _description = "Vehicle Type"
 
     name = fields.Char(string="Type Name", required=True)
+
+    _sql_constraints = [
+        ('unique_vehicle_type_name', 'unique(name)', 'Vehicle type name must be unique!')
+    ]
+
+    @api.constrains('name')
+    def _check_unique_name(self):
+        for rec in self:
+            if not rec.name:
+                continue
+
+            domain = [
+                ('id', '!=', rec.id),
+                ('name', '=ilike', rec.name.strip()),
+            ]
+            if self.search_count(domain):
+                raise ValidationError(
+                    f'The vehicle type "{rec.name}" already exists.'
+                )
